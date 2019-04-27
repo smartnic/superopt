5b7e7b352d33294da77caaebc01a4d444402e8ed
Srinivas Narayana <ngsrinivas@gmail.com>
2019-04-26 22:59:36 -0400

    Implement basic MH iterations for a single variable

--- mh.cc
+++ mh.cc
@@ -1,40 +1,66 @@
 #include <iostream>
 #include <random>
+#include <algorithm>
+#include <string>
+
+#define PDF_SUPPORT 40
 
 using namespace std;
 
 default_random_engine gen;
+uniform_real_distribution<double> unidist(0.0, 1.0);
 
 /* Function that provides the probability density given a support variable x */
 double pi(double x) {
-  if (x > 0.01) {
-    return 1.0/x;
-  } else {
-    return 100.0;
-  }
+  double center = 15.0;
+  if (x > center)
+    return max(0.0, (2.0 * center) - x);
+  else
+    return max(0.0, x);
 }
 
-/* Proposal distribution */
+/* Generate a new sample from proposal distribution */
 double generate_y(double x) {
   normal_distribution<double> dist(x, 2.0);
   double y = dist(gen);
   return y;
 }
 
+/* compute acceptance function */
+double alpha(double x, double y) {
+  /* Use the simplified form when proposal distribution is symmetric */
+  return min(1.0, pi(y) / pi(x));
+}
+
+/* Get the next MCMC sample */
+double mh_next(double x) {
+  double y = generate_y(x);
+  double uni_sample = unidist(gen);
+  if (uni_sample < alpha(x, y))
+    return y;
+  else
+    return x;
+}
+
 int main() {
   double x = 10.0;
   double y;
-  int p[20] = {};
+  int p[PDF_SUPPORT] = {};
+  int nrolls = 100000;
+  int nstars = 100;
 
   cout << "pi " << pi(x) << endl << "Dist computed:" << endl;
 
-  for (int i=0; i<10000; i++) {
-    y = generate_y(x);
-    if (y >= 0 && y < 20) ++p[int(y)];
+  /* MH iterations */
+  for (int i=0; i<nrolls; i++) {
+    y = mh_next(x);
+    if (y >= 0 && y < PDF_SUPPORT) ++p[int(y)];
+    x = y;
   }
-  
-  for (int i=0; i<20; i++)
-    cout << i << ": " << p[i] << endl;
+
+  /* Print the distribution */
+  for (int i=0; i<PDF_SUPPORT; i++)
+    cout << i << ": " << string(p[i] * nstars/nrolls, '*') << endl;
   
   return 0;
 }
