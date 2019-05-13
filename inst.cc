bd0683d0c8732085b258822958c561916ace91fc
Srinivas Narayana <ngsrinivas@gmail.com>
2019-05-13 11:56:23 -0400

    Replaced classes by jumptable-based interpretation

--- inst.cc
+++ inst.cc
@@ -8,7 +8,6 @@ class prog_state {
   int pc = 0; /* Assume only straight line code execution for now */
  public:
   int regs[NUM_REGS] = {}; /* assume only registers for now */
-  bool halted = false; /* machine initially starts off as not halted */
   void print();
 };
 
@@ -20,98 +19,74 @@ void prog_state::print() {
 
 class inst {
  public:
-  inst() = default;
-  virtual int interpret(prog_state &ps) { return 0; };
-};
-
-class addxy : public inst {
-  int _x, _y;
- public:
-  addxy(int x, int y) { _x = x; _y = y; }
-  int interpret(prog_state &ps);
-};
-
-class movxc : public inst {
-  int _x, _c;
- public:
-  movxc(int x, int c) { _x = x; _c = c; }
-  int interpret(prog_state &ps);
-};
-
-class retx: public inst {
-  int _x;
- public:
-  retx(int x) { _x = x; }
-  int interpret(prog_state &ps);
-};
-
-class retc: public inst {
-  int _c;
- public:
-  retc(int c) { _c = c; }
-  int interpret(prog_state &ps);
-};
-
-int addxy::interpret(prog_state &ps) {
-  if (! ps.halted) {
-    ps.regs[_x] = ps.regs[_x] + ps.regs[_y];
-    return ps.regs[_x];
-  } else {
-    return -1;
+  int _opcode;
+  int _arg1;
+  int _arg2;
+  int _jmp_off;
+  inst(int opcode, int arg1, int arg2=0, int jmp_off=0) {
+    _opcode  = opcode;
+    _arg1    = arg1;
+    _arg2    = arg2;
+    _jmp_off = jmp_off;
   }
+};
+
+// Instruction opcodes
+#define ADDXY 0
+#define MOVXC 1
+#define RETX 2
+#define RETC 3
+
+int interpret(inst *program, int length, prog_state &ps) {
+  inst *insn = program;
+
+  static void *jumptable[256] = {
+    [ADDXY] = &&INSN_ADDXY,
+    [MOVXC] = &&INSN_MOVXC,
+    [RETX] = &&INSN_RETX,
+    [RETC] = &&INSN_RETC,
+    [4 ... 255] = &&error_label,
+  };
+
+#define CONT ({ ps.print(); insn++; if (insn < program + length) goto select_insn; })
+#define DST ps.regs[insn->_arg1]
+#define SRC ps.regs[insn->_arg2]
+#define IMM1 insn->_arg1
+#define IMM2 insn->_arg2
+#define JMP_OFF ps.regs[insn->_jmp_off]
+
+select_insn:
+  goto *jumptable[insn->_opcode];
+
+INSN_ADDXY:
+  DST = DST + SRC;
+  CONT;
+
+INSN_MOVXC:
+  DST = IMM2;
+  CONT;
+
+INSN_RETX:
+  return DST;
+
+INSN_RETC:
+  return IMM1;
+
+error_label:
+  cout << "Error in processing instruction; unknown opcode" << endl;
+  return -1;
 }
-
-int movxc::interpret(prog_state &ps) {
-  if (! ps.halted) {
-    ps.regs[_x] = _c;
-    return ps.regs[_x];
-  } else {
-    return -1;
-  }
-}
-
-int retx::interpret(prog_state &ps) {
-  if (! ps.halted) {
-    ps.halted = true;
-    return ps.regs[_x];
-  } else {
-    return -1;
-  }
-}
-
-int retc::interpret(prog_state &ps) {
-  if (! ps.halted) {
-    ps.halted = true;
-    return _c;
-  } else {
-    return -1;
-  }
-};
 
 int main() {
-  movxc mov1(1, 10);
-  movxc mov2(2, 4);
-  addxy add(1, 2);
-  retx ret(1);
-  movxc mov3(2, 5);
-  inst instructions[5] = {mov1, mov2, add, ret, mov3};
+  inst instructions[6] = {inst(MOVXC, 1, 10), /* mov r1, 10 */
+                          inst(MOVXC, 2, 4),  /* mov r2, 4  */
+                          inst(ADDXY, 1, 2),  /* add r1, r2 */
+                          inst(MOVXC, 3, 5),  /* mov r3, 5  */
+                          inst(ADDXY, 1, 3),  /* add r1, r3 */
+                          inst(RETX, 1),      /* ret r1     */
+  };
   prog_state ps;
 
-  // for (int i = 0; i < 5; i++ ) {
-  //   cout << "Result of instruction " << i << ": " <<
-  //       dynamic_cast<void*>(&instructions[i]).interpret(ps) << endl;
-  //   ps.print();
-  // }
-
-  cout << "Result of first move:" << mov1.interpret(ps) << endl;
-  ps.print();
-  cout << "Result of second move:" << mov2.interpret(ps) << endl;
-  ps.print();
-  cout << "Result of in-place addition:" << add.interpret(ps) << endl;
-  ps.print();
-  cout << "Result of return:" << ret.interpret(ps) << endl;
-  ps.print();
-  cout << "Result of third move:" << mov3.interpret(ps) << endl;
-  ps.print();
+  cout << "Result of full interpretation: " << interpret(instructions, 6, ps) << endl;
   return 0;
 }
