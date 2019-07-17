5f149adf69d5b0904ef47d26e19e1bed9a16fb82
Srinivas Narayana <ngsrinivas@gmail.com>
2019-07-17 05:38:20 +0530

    Create UNUSED_OPS macro

--- inst.h
+++ inst.h
@@ -56,6 +56,7 @@ class inst {
 #define OPTYPE(opcode, opindex) ((optable[opcode] >> ((opindex-1) * 5)) & 31)
 
 #define JMP_OPS (FSTOP(OP_REG) | SNDOP(OP_REG) | TRDOP(OP_OFF))
+#define UNUSED_OPS (FSTOP(OP_UNUSED) | SNDOP(OP_UNUSED) | TRDOP(OP_UNUSED))
 
 static int optable[256] = {
   [NOP]   = UNUSED_OPS,
