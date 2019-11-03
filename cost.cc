88232252fe340f99575891d29efb61cde464af05
QiongwenXu <qwxu@outlook.com>
2019-11-03 14:43:15 -0500

    support class cost

--- cost.cc
+++ cost.cc
@@ -4,6 +4,7 @@
 #include <string>
 #include "inst.h"
 #include "inout.h"
+#include "cost.h"
 
 /* Requires support for advanced bit manipulation (ABM) instructions on the
  * architecture where this program is run. */
@@ -13,44 +14,53 @@ unsigned int pop_count_asm(unsigned int x) {
   asm ("popcnt %1, %0"
        : "=a" (z)
        : "b" (y)
-       );
+      );
   return z;
 }
 
-/* Compute correctness error metric between two programs on outputs */
-int error_cost(inout* examples, int num_ex, inst* orig,
-               inst* synth, bool exec_orig=false) {
-  double total_cost = 0;
-  prog_state ps;
-  int output1, output2;
-  for (int i = 0; i < num_ex; i++) {
-    if (exec_orig)
-      output1 = interpret(orig,  MAX_PROG_LEN, ps, examples[i].input);
-    else
-      output1 = examples[i].output;
-    output2 = interpret(synth, MAX_PROG_LEN, ps, examples[i].input);
-    cout << "Expected output: " << output1 << " Got output " << output2 << endl;
-    // int ex_cost = pop_count_asm(output1 ^ output2);
-    int ex_cost = abs(output1 - output2);
-    total_cost += ex_cost;
-    if (output1 != examples[i].output)
-      cout << "Error: Original program output does not match provided "
-          "input-output pair" << endl;
-  }
-  if (num_ex > 0) return (int)(total_cost);
-  else return 0;
-}
+cost::cost() {}
 
-int num_real_instructions(inst* program) {
+cost::~cost() {}
+
+int cost::num_real_instructions(inst* program, int len) {
   int count = 0;
-  for (int i=0; i < MAX_PROG_LEN; i++) {
+  for (int i = 0; i < len; i++) {
     if (program[i]._opcode != NOP) count++;
   }
   return count;
 }
 
-int perf_cost(inout* examples, int num_ex, inst* orig,
-              inst* synth) {
-  return MAX_PROG_LEN - num_real_instructions(orig) +
-      num_real_instructions(synth);
+void cost::set_orig(inst* orig, int len) {
+  _vld.set_orig(orig, len);
+  _num_real_orig = num_real_instructions(orig, len);
+}
+
+int cost::error_cost(inst* synth, int len) {
+  double total_cost = 0;
+  prog_state ps;
+  int output1, output2;
+  for (int i = 0; i < _examples._exs.size(); i++) {
+    output1 = _examples._exs[i].output;
+    output2 = interpret(synth, len, ps, _examples._exs[i].input);
+    // cout << "Expected output: " << output1 << " Got output " << output2 << endl;
+    // int ex_cost = pop_count_asm(output1 ^ output2);
+    int ex_cost = abs(output1 - output2);
+    if (!ex_cost) {
+      ex_cost = 1 - (int)_vld.is_equal_to(synth, len);
+    }
+    total_cost += ex_cost;
+  }
+  return (int)(total_cost);
+}
+
+int cost::perf_cost(inst* synth, int len) {
+  return MAX_PROG_LEN - _num_real_orig + num_real_instructions(synth, len);
+}
+
+double cost::total_prog_cost(inst* synth, int len) {
+  double err_cost = error_cost(synth, len);
+  cout << "Error cost: " << err_cost << endl;
+  double per_cost = perf_cost(synth, len);
+  cout << "Perf cost: " << per_cost << endl;
+  return (_w_e * err_cost) + (_w_p * per_cost);
 }
--- cost.h
+++ cost.h
@@ -1,8 +1,28 @@
+#pragma once
+
+#include <vector>
 #include "inst.h"
 #include "inout.h"
+#include "validator.h"
+#include "ex.h"
 
 using namespace std;
 
 unsigned int pop_count_asm(unsigned int);
-int error_cost(inout*, int, inst*, inst*, bool=false);
-int perf_cost(inout*, int, inst*, inst*);
+
+class cost {
+ private:
+  int _num_real_orig;
+  int num_real_instructions(inst* program, int len);
+ public:
+  validator _vld;
+  examples _examples;
+  double _w_e = 0.5;
+  double _w_p = 0.5;
+  cost();
+  ~cost();
+  void set_orig(inst* orig, int len);
+  int error_cost(inst* synth, int len);
+  int perf_cost(inst* synth, int len);
+  double total_prog_cost(inst* synth, int len);
+};
