7da1e05b05ac615bb3e7eddcc9baa23ded427e97
QiongwenXu <qwxu@outlook.com>
2019-11-07 01:16:21 -0500

    separate the processing steps in error cost computation
    steps:
    1. process error cost with example set;
    2. process error cost with verifying result;
    3. insert counterexamples into example set
    
    Still NEED TO CHECK whether the counterexample has already been in the example set.
    For examples, if the example set is {(10,15)}, the output of synth is 10, then (10,15)is also an counterexample, which shows that example chek cannot ensure the same counterexample generation, since the example that synth fails in the example set is a counterexample.

--- cost.cc
+++ cost.cc
@@ -62,40 +62,51 @@ int cost::error_cost(prog* synth, int len) {
   inst* inst_list = (inst*)synth->inst_list;
   prog_state ps;
   int output1, output2;
-  examples counterexs;
+  int verify_times = 0;
+  // process total_cost with example set
   for (int i = 0; i < _examples._exs.size(); i++) {
     output1 = _examples._exs[i].output;
     output2 = interpret(inst_list, len, ps, _examples._exs[i].input);
     cout << "Expected output: " << output1 << " Got output " << output2 << endl;
     // int ex_cost = pop_count_asm(output1 ^ output2);
     int ex_cost = abs(output1 - output2);
-    if (!ex_cost) {
-      int is_equal = 0;
-      if (synth->_verfiy_res_flag) {
-        is_equal = synth->_verfiy_res;
-      } else {
-        is_equal = _vld.is_equal_to(inst_list, len);
-        synth->set_verify_res(is_equal);
-      }
-      if (is_equal == 0) { // not equal
-        counterexs.insert(_vld._last_counterex);
-        ex_cost = 1;
-      } else if (is_equal == 1) { // equal
-        ex_cost = 0;
-      } else { // synth illegal
-        total_cost = ERROR_COST_MAX;
-        synth->set_error_cost((int)total_cost);
-        break;
-      }
-      cout << "is_equal: " << is_equal << endl;
-    }
+    if (! ex_cost) verify_times++;
     total_cost += ex_cost;
   }
-  synth->set_error_cost((int)total_cost);
-  for (size_t i = 0; i < counterexs._exs.size(); i++) {
-    _examples.insert(counterexs._exs[i]);
+  // if verifying is needed, process total_cost with verify result
+  int is_equal = 0;
+  if (verify_times > 0) {
+    if (synth->_verfiy_res_flag) {
+      is_equal = synth->_verfiy_res;
+    } else {
+      is_equal = _vld.is_equal_to(inst_list, len);
+      synth->set_verify_res(is_equal);
+    }
+    // equal: total_cost += 0
+    // not equal: total_cost += verify_times * 1
+    // synth illegal: total_cost = ERROR_COST_MAX
+    if (is_equal == 0) { // not equal
+      total_cost += verify_times;
+    } else if (is_equal == -1) { // synth illegal
+      total_cost = ERROR_COST_MAX;
+    }
   }
-  if (counterexs._exs.size() > 0)
+  synth->set_error_cost((int)total_cost);
+
+  bool gen_counterex_flag = (is_equal == 0);
+  // If verify_times < (int)_examples._exs.size(),
+  // it shows the example that synth fails in the example set is a counterexample.
+  // The counterexample generated from this synth may have already been in the examples set.
+  // Thus, only when verify_times == (int)_examples._exs.size(),
+  // the counterexample generated from this synth must can be added into the example set.
+  // But it should ensure that the number of initial example set is big enough. 
+  // bool gen_counterex_flag = (is_equal == 0) && (verify_times == (int)_examples._exs.size());
+
+  // process counterexamples
+  if (gen_counterex_flag) {
+    _examples.insert(_vld._last_counterex);
+  }
+  if (gen_counterex_flag)
     cout << "new example set is:\n" << _examples._exs << endl;
   return (int)(total_cost);
 }
