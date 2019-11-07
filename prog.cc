96d3dc0ebca7219248bb591eeaa66bfb2cc4a6a5
QiongwenXu <qwxu@outlook.com>
2019-11-07 09:22:54 -0500

    rm flags from class prog and set the initial variables as -1

--- prog.cc
+++ prog.cc
@@ -5,11 +5,8 @@ using namespace std;
 // TODO: find canonical way to invoke one constructor from another
 prog::prog(const prog& other) {
   freq_count = other.freq_count;
-  _verfiy_res_flag = other._verfiy_res_flag;
-  _verfiy_res = other._verfiy_res;
-  _error_cost_flag = other._error_cost_flag;
+  _verify_res = other._verify_res;
   _error_cost = other._error_cost;
-  _perf_cost_flag = other._perf_cost_flag;
   _perf_cost = other._perf_cost;
   for (int i=0; i < MAX_PROG_LEN; i++) {
     inst_list[i] = other.inst_list[i];
@@ -18,12 +15,9 @@ prog::prog(const prog& other) {
 
 prog::prog(inst* instructions) {
   freq_count = 0;
-  _verfiy_res_flag = false;
-  _verfiy_res = 0;
-  _error_cost_flag = false;
-  _error_cost = 0;
-  _perf_cost_flag = false;
-  _perf_cost = 0;
+  _verify_res = -1;
+  _error_cost = -1;
+  _perf_cost = -1;
   for (int i=0; i < MAX_PROG_LEN; i++) {
     inst_list[i] = instructions[i];
   }
@@ -33,12 +27,9 @@ prog::prog(inst* instructions) {
 prog* prog::make_prog(const prog &other) {
   prog* new_prog = (prog*)malloc(sizeof(prog));
   new_prog->freq_count = 0;
-  new_prog->_verfiy_res_flag = false;
-  new_prog->_verfiy_res = 0;
-  new_prog->_error_cost_flag = false;
-  new_prog->_error_cost = 0;
-  new_prog->_perf_cost_flag = false;
-  new_prog->_perf_cost = 0;
+  new_prog->_verify_res = -1;
+  new_prog->_error_cost = -1;
+  new_prog->_perf_cost = -1;
   for (int i=0; i < MAX_PROG_LEN; i++) {
     new_prog->inst_list[i] = other.inst_list[i];
   }
@@ -72,17 +63,14 @@ bool prog::operator==(const prog &x) const {
 }
 
 void prog::set_verify_res(int res) {
-  _verfiy_res_flag = true;
-  _verfiy_res = res;
+  _verify_res = res;
 }
 
 void prog::set_error_cost(int cost) {
-  _error_cost_flag = true;
   _error_cost = cost;
 }
 
 void prog::set_perf_cost(int cost) {
-  _perf_cost_flag = true;
   _perf_cost = cost;
 }
 
