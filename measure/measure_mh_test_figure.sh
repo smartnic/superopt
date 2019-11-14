3b4c12030614998a01acc3c6c7bc4b29d4136161
QiongwenXu <qwxu@outlook.com>
2019-11-14 11:13:37 -0500

    modify figure.sh

--- measure/measure_mh_test_figure.sh
+++ measure/measure_mh_test_figure.sh
@@ -1,3 +1,4 @@
 pythonfile="measure/measure_mh_test_figure.py"
-test="measure/"
-python3 ${pythonfile} ${test} ${test} 0.5,1.5 0 4,2,4,4 10000
+file_in="measure/"
+file_out=${file_in}
+python3 ${pythonfile} ${file_in} ${file_out} 0.5,1.5 0 4,2,4,4 10000
