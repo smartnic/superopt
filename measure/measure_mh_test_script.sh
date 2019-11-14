987e5aea17038e6b1110340a4ed5008d6e719238
QiongwenXu <qwxu@outlook.com>
2019-11-14 10:59:09 -0500

    rename measure_mh_test_script.sh

---
+++ measure/measure_mh_test_script.sh
@@ -0,0 +1,4 @@
+path="measure/"
+path_exe="measure/"
+iteration=20000
+./${path_exe}measure_mh_test.out 0 ${iteration} 0.5 1.5 ${path} 0 0 1 > ${path}progs_0_0.5_1.5.txt
--- measure/measure_time_test_script.sh
+++
@@ -1,4 +0,0 @@
-path="measure/"
-path_exe="measure/"
-iteration=1000
-./${path_exe}measure_mh_test.out 0 ${iteration} 0.5 0.5 ${path} 0 0 0 > ${path}progs_0_0.5_0.5.txt
