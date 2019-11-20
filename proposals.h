9130d7e0df7bfc42c508ab518589e289c9a37e3b
QiongwenXu <qwxu@outlook.com>
2019-11-20 11:15:43 -0500

    proposal support modify random k continuous instructions

--- proposals.h
+++ proposals.h
@@ -5,3 +5,4 @@ using namespace std;
 
 prog* mod_random_inst_operand(const prog &program);
 prog* mod_random_inst(const prog &program);
+prog* mod_random_k_cont_insts(const prog &program, unsigned int k);
