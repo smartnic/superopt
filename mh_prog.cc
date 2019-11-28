2204e6fa077502f7b898cc93a54501356ea63652
QiongwenXu <qwxu@outlook.com>
2019-11-28 16:50:55 -0500

    add different mh sampler strategies
    1. add `when to start` strategy
    2. add `next start program` strategy
    3. add `next proposal` strategy

--- mh_prog.cc
+++ mh_prog.cc
@@ -14,6 +14,100 @@ using namespace std;
 default_random_engine gen_mh;
 uniform_real_distribution<double> unidist_mh(0.0, 1.0);
 
+/* class mh_sampler_when_to_restart start */
+mh_sampler_when_to_restart::mh_sampler_when_to_restart() {
+  set_st_no_restart();
+}
+
+mh_sampler_when_to_restart::~mh_sampler_when_to_restart() {}
+
+void mh_sampler_when_to_restart::set_st_no_restart() {
+  cout << "set strategy MH_SAMPLER_ST_WHEN_TO_RESTART_NO_RESTART" << endl;
+  _st = MH_SAMPLER_ST_WHEN_TO_RESTART_NO_RESTART;
+}
+
+void mh_sampler_when_to_restart::set_st_max_iter(unsigned int max_num_iter) {
+  cout << "set strategy MH_SAMPLER_ST_WHEN_TO_RESTART_MAX_ITER with max_num_iter "
+       << max_num_iter << endl;
+  _st = MH_SAMPLER_ST_WHEN_TO_RESTART_MAX_ITER;
+  _max_num_iter = max_num_iter;
+}
+
+bool mh_sampler_when_to_restart::whether_to_restart(unsigned int iter_num) {
+  switch (_st) {
+    case MH_SAMPLER_ST_WHEN_TO_RESTART_NO_RESTART: return false;
+    case MH_SAMPLER_ST_WHEN_TO_RESTART_MAX_ITER:
+      // iter_num starts from 0 but not 1
+      if (((iter_num + 1) % _max_num_iter) == 0) return true;
+      else return false;
+    default:
+      cout << "ERROR: no when_to_restart strategy matches." << endl;
+      return false;
+  }
+}
+/* class mh_sampler_when_to_restart end */
+
+/* class mh_sampler_next_start_prog start */
+mh_sampler_next_start_prog::mh_sampler_next_start_prog() {
+  set_st_orig();
+}
+
+mh_sampler_next_start_prog::~mh_sampler_next_start_prog() {}
+
+void mh_sampler_next_start_prog::set_st_orig() {
+  cout << "set strategy MH_SAMPLER_ST_NEXT_START_PROG_ORIG" << endl;
+  _st = MH_SAMPLER_ST_NEXT_START_PROG_ORIG;
+}
+
+void mh_sampler_next_start_prog::set_st_all_insts() {
+  cout << "set strategy MH_SAMPLER_ST_NEXT_START_PROG_ALL_INSTS" << endl;
+  _st = MH_SAMPLER_ST_NEXT_START_PROG_ALL_INSTS;
+}
+
+void mh_sampler_next_start_prog::set_st_k_cont_insts() {
+  cout << "set strategy MH_SAMPLER_ST_NEXT_START_PROG_K_CONT_INSTS" << endl;
+  _st = MH_SAMPLER_ST_NEXT_START_PROG_K_CONT_INSTS;
+}
+
+prog* mh_sampler_next_start_prog::next_start_prog(prog* curr) {
+  switch (_st) {
+    case MH_SAMPLER_ST_NEXT_START_PROG_ORIG:
+      return curr;
+    case MH_SAMPLER_ST_NEXT_START_PROG_ALL_INSTS:
+      return mod_random_k_cont_insts(*curr, MAX_PROG_LEN);
+    case MH_SAMPLER_ST_NEXT_START_PROG_K_CONT_INSTS:
+      return mod_random_cont_insts(*curr);
+    default:
+      cout << "ERROR: no next_start_prog strategy matches." << endl;
+      return curr;
+  }
+}
+/* class mh_sampler_next_start_prog end */
+
+/* class mh_sampler_next_proposal start */
+mh_sampler_next_proposal::mh_sampler_next_proposal() {
+  set_st_inst();
+}
+
+mh_sampler_next_proposal::~mh_sampler_next_proposal() {}
+
+void mh_sampler_next_proposal::set_st_inst() {
+  cout << "set strategy MH_SAMPLER_ST_NEXT_PROPOSAL_INST" << endl;
+  _st = MH_SAMPLER_ST_NEXT_PROPOSAL_INST;
+}
+
+prog* mh_sampler_next_proposal::next_proposal(prog* curr) {
+  switch (_st) {
+    case MH_SAMPLER_ST_NEXT_PROPOSAL_INST:
+      return mod_random_inst(*curr);
+    default:
+      cout << "ERROR: no next_proposal strategy matches." << endl;
+      return mod_random_inst(*curr);
+  }
+}
+/* class mh_sampler_next_proposal end */
+
+/* class mh_sampler start */
 mh_sampler::mh_sampler() {
   _meas_data._mode = false;
 }
@@ -24,13 +118,6 @@ double mh_sampler::cost_to_pi(double cost) {
   return pow(_base, -1.0 * cost);
 }
 
-prog* mh_sampler::next_proposal(prog* curr) {
-  // TODO: use proposal generating functions to generate new programs
-  // Use mod_random_inst_operand(.) and mod_random_inst(.) to do so
-  // return mod_random_inst_operand(*curr);
-  return mod_random_inst(*curr);
-}
-
 /* compute acceptance function */
 double mh_sampler::alpha(prog* curr, prog* next) {
   double curr_cost = _cost.total_prog_cost(curr, MAX_PROG_LEN);
@@ -39,7 +126,7 @@ double mh_sampler::alpha(prog* curr, prog* next) {
 }
 
 prog* mh_sampler::mh_next(prog* curr) {
-  prog* next = next_proposal(curr);
+  prog* next = _next_proposal.next_proposal(curr);
   double uni_sample = unidist_mh(gen_mh);
   double a = alpha(curr, next);
   _meas_data.insert_proposal(*next, uni_sample < a);
@@ -58,40 +145,63 @@ prog* mh_sampler::mh_next(prog* curr) {
   }
 }
 
-void mh_sampler::mcmc_iter(int niter, const prog &orig,
-                           unordered_map<int, vector<prog*> > &prog_freq) {
-  // contruct the first program by copying the original
-  prog *curr, *next;
-  curr = prog::make_prog(start);
-
-  for (int i = 0; i < niter; i++) {
-    next = mh_next(curr);
-    /* Insert the next program into frequency dictionary if new */
-    int ph = progHash()(*next);
-    prog* meas_p;
-    bool found = false;
-    if (prog_freq.find(ph) == prog_freq.end()) {
-      prog_freq[ph] = std::vector<prog*>();
-    } else {
-      vector<prog*> chain = prog_freq[ph];
-      for (auto p : chain) {
-        if (*p == *next) {
-          found = true;
-          p->freq_count++;
-          meas_p = p;
-          break;
-        }
+/* Insert the next program into frequency dictionary if new */
+void insert_into_prog_freq_dic(const prog &next,
+                               unordered_map<int, vector<prog*> > &prog_freq) {
+  int ph = progHash()(next);
+  prog* meas_p;
+  bool found = false;
+  if (prog_freq.find(ph) == prog_freq.end()) {
+    prog_freq[ph] = std::vector<prog*>();
+  } else {
+    vector<prog*> chain = prog_freq[ph];
+    for (auto p : chain) {
+      if (*p == next) {
+        found = true;
+        p->freq_count++;
+        meas_p = p;
+        break;
       }
     }
-    if (! found) {
-      // TODO: may define new prog copy API
-      prog* next_copy = prog::make_prog(*next);
-      next_copy->freq_count++;
-      next_copy->_error_cost = next->_error_cost;
-      next_copy->_perf_cost = next->_perf_cost;
-      prog_freq[ph].push_back(next_copy);
-      meas_p = next_copy;
+  }
+  if (! found) {
+    // TODO: may define new prog copy API
+    prog* next_copy = prog::make_prog(next);
+    next_copy->freq_count++;
+    next_copy->_error_cost = next._error_cost;
+    next_copy->_perf_cost = next._perf_cost;
+    prog_freq[ph].push_back(next_copy);
+    meas_p = next_copy;
+  }
+}
+
+void mh_sampler::print_restart_info(int iter_num, const prog &curr, const prog &restart) {
+  cout << "restart at iteration " << iter_num << endl;
+  cout << "  current program" << endl;
+  curr.print(curr);
+  cout << "  restart program" << endl;
+  restart.print(restart);
+}
+
+void mh_sampler::mcmc_iter(int niter, const prog &orig,
+                           unordered_map<int, vector<prog*> > &prog_freq) {
+  prog *curr, *next;
+  curr = prog::make_prog(orig);
+  for (int i = 0; i < niter; i++) {
+    // check whether need restart, if need, update `start`
+    if (_when_to_restart.whether_to_restart(i)) {
+      prog *restart = _start_prog.next_start_prog(curr);
+      print_restart_info(i, *curr, *restart);
+      if (curr != restart) {
+        prog::clear_prog(curr);
+        curr = restart;
+      }
     }
+    // sample one program
+    next = mh_next(curr);
+    // insert the next program into frequency dictionary of programs
+    insert_into_prog_freq_dic(*next, prog_freq);
+    // update measurement data and update current program with the next program
     if (i == 0) {
       _meas_data.insert_program(i, *curr);
     }
@@ -112,47 +222,3 @@ void mh_sampler::turn_off_measure() {
   _meas_data._mode = false;
 }
 /* class mh_sampler_restart end */
-
-/* class mh_sampler_restart start */
-mh_sampler_restart::mh_sampler_restart(int max_iteration) {
-  _max_iteration = max_iteration;
-}
-
-mh_sampler_restart::~mh_sampler_restart() {}
-
-prog* mh_sampler_restart::next_restart_proposal(prog *curr) {
-  return mod_random_k_cont_insts(*curr, MAX_PROG_LEN);
-}
-
-void mh_sampler_restart::mcmc_iter(int niter, const prog &orig,
-                                   unordered_map<int, vector<prog*> > &prog_freq) {
-  prog *curr, *next;
-  curr = prog::make_prog(orig);
-  int num_max_iter = niter / _max_iteration;
-  int num_remain_iter = niter % _max_iteration;
-  prog *start;
-  start = prog::make_prog(orig);
-  for (int i = 0; i < num_max_iter; i++) {
-    mh_sampler::mcmc_iter(_max_iteration, *start, prog_freq);
-    prog *restart = next_restart_proposal(start);
-    prog::clear_prog(start);
-    start = restart;
-    // cout << "restart:\n";
-    // start->print();
-  }
-  mh_sampler::mcmc_iter(num_remain_iter, *start, prog_freq);
-  prog::clear_prog(start);
-}
-/* class mh_sampler_restart end */
-
-/* class mh_sampler_k_restart start */
-mh_sampler_k_restart::mh_sampler_k_restart(int max_iteration) {
-  _max_iteration = max_iteration;
-}
-
-mh_sampler_k_restart::~mh_sampler_k_restart() {}
-
-prog* mh_sampler_k_restart::next_restart_proposal(prog *curr) {
-  return mod_random_cont_insts(*curr);
-}
-/* class mh_sampler_k_restart end */
