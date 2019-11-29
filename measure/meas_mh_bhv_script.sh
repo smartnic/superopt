29a6feebdddbd74bef3d30f4be5c73806d2ed01b
QiongwenXu <qwxu@outlook.com>
2019-11-29 03:10:02 -0500

    add mh strategy into measure

--- measure/meas_mh_bhv_script.sh
+++ measure/meas_mh_bhv_script.sh
@@ -1,1 +1,7 @@
-measure/meas_mh_bhv.out -n 10 --path_out "measure/" --bm 2 --we 1.0 --wp 0.5 --st_ex 0 --st_eq 1 --st_avg 0
+measure/meas_mh_bhv.out \
+-n 10 --path_out "measure/" --bm 2 \
+--we 1.0 --wp 0.5 \
+--st_ex 0 --st_eq 1 --st_avg 0 \
+--st_when_to_restart 1 --st_when_to_restart_niter 5 \
+--st_start_prog 2  \
+--p_inst_operand 0.33 --p_inst 0.34
