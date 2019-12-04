961838ee15d6e7f06e8ef1a8130bbbf8e3f1f842
QiongwenXu <qwxu@outlook.com>
2019-12-04 11:47:17 -0500

    modify script according to the modifcation of input parameters add and change

--- measure/meas_mh_bhv_script.sh
+++ measure/meas_mh_bhv_script.sh
@@ -1,7 +1,8 @@
 ./main.out \
--m -n 5000 --meas_path_out "measure/" --bm 0 \
---we 0.5 --wp 1.5 \
+-m -n 5000 --path_out "measure/" --bm 0 \
+--w_e 0.5 --w_p 1.5 \
 --st_ex 0 --st_eq 0 --st_avg 0 \
---st_when_to_restart 0 --st_when_to_restart_niter 0 \
+--st_when_to_restart 1 --st_when_to_restart_niter 2000 \
 --st_start_prog 0  \
+--restart_w_e_list 0.5,1.5 --restart_w_p_list 1.5,0.5 \
 --p_inst_operand 0.33 --p_inst 0.34
