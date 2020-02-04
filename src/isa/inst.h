dbf4f66f540463dfdf944b3de5da79ccc338eae0
QiongwenXu <qwxu@outlook.com>
2020-02-04 12:57:23 -0500

    if functions in class inst have not been implemented, raise exception

--- src/isa/inst.h
+++ src/isa/inst.h
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-enum ISA_TYPES{
+enum ISA_TYPES {
   TOY_ISA = 0,
   EBPF,
 };
@@ -31,6 +31,12 @@ class prog_state_base {
   void clear();
 };
 
+#define RAISE_EXCEPTION(x) {\
+  string err_msg = string(x) + string(" has not been implemented"); \
+  cerr << err_msg << endl;\
+  throw (err_msg); \
+}
+
 class inst_base {
  public:
   int _opcode;
@@ -43,29 +49,29 @@ class inst_base {
   void set_opcode(int op_value);
 
   /* Functions class inst should support */
-  // bool operator==(const inst &x) const;
-  // void print() const;
-  // vector<int> get_reg_list() const;
-  // virtual string opcode_to_str(int) const {return "";}
-  // virtual op_t get_max_operand_val(int op_index, int inst_index = 0) const {return 0;}
-  // virtual int get_jmp_dis() const {return 0;}
-  // // insert all jmp opcode in jmp_set, used by proposals.cc to 
-  // // avoid jumps in the last line of the program
-  // virtual void insert_jmp_opcodes(unordered_set<int>& jmp_set) const {}
-  // virtual int inst_output_opcode_type() const {return 0;}
-  // virtual int inst_output() const {return 0;}
-  // virtual bool is_real_inst() const {return false;}
-  // virtual void set_as_nop_inst() {}
-  // virtual unsigned int get_input_reg() const {return 0;}
-  // // for class toy_isa
-  // virtual int get_num_operands() const {return 0;}
-  // virtual int get_insn_num_regs() const {return 0;}
-  // virtual int get_opcode_type() const {return 0;}
-  // // smt
-  // // return SMT for the given OP_OTHERS type instruction, other types return false
-  // virtual z3::expr smt_inst(smt_var& sv) const {return string_to_expr("false");}
-  // // return SMT for the given OP_COND_JMP type instruction, other types return false
-  // virtual z3::expr smt_inst_jmp(smt_var& sv) const {return string_to_expr("false");}
+  // inst& operator=(const inst &rhs)
+  bool operator==(const inst_base &x) const {RAISE_EXCEPTION("inst::operator==");}
+  void print() const {RAISE_EXCEPTION("inst::print");}
+  vector<int> get_reg_list() const {RAISE_EXCEPTION("inst::get_reg_list");}
+  string opcode_to_str(int) const {RAISE_EXCEPTION("inst::opcode_to_str");}
+  op_t get_max_operand_val(int op_index, int inst_index = 0) const {RAISE_EXCEPTION("inst::get_max_operand_val");}
+  int get_jmp_dis() const {RAISE_EXCEPTION("inst::get_jmp_dis");}
+  // insert all jmp opcode in jmp_set, used by proposals.cc to
+  // avoid jumps in the last line of the program
+  void insert_jmp_opcodes(unordered_set<int>& jmp_set) const {RAISE_EXCEPTION("inst::insert_jmp_opcodes");}
+  int inst_output_opcode_type() const {RAISE_EXCEPTION("inst::inst_output_opcode_type");}
+  int inst_output() const {RAISE_EXCEPTION("inst::inst_output");}
+  bool is_real_inst() const {RAISE_EXCEPTION("inst::is_real_inst");}
+  void set_as_nop_inst() {RAISE_EXCEPTION("inst::set_as_nop_inst");}
+  unsigned int get_input_reg() const {RAISE_EXCEPTION("inst::get_input_reg");}
+  int get_num_operands() const {RAISE_EXCEPTION("inst::get_num_operands");}
+  int get_insn_num_regs() const {RAISE_EXCEPTION("inst::get_insn_num_regs");}
+  int get_opcode_type() const {RAISE_EXCEPTION("inst::get_opcode_type");}
+  // smt
+  // return SMT for the given OP_OTHERS type instruction, other types return false
+  z3::expr smt_inst(smt_var& sv) const {RAISE_EXCEPTION("inst::smt_inst");}
+  // return SMT for the given OP_COND_JMP type instruction, other types return false
+  z3::expr smt_inst_jmp(smt_var& sv) const {RAISE_EXCEPTION("inst::smt_inst_jmp");}
 };
 
 struct instHash {
