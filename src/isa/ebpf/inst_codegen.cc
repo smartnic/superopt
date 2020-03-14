33ee6678fd6e9e870ce2c9d5c06cce0765898f35
QiongwenXu <qwxu@outlook.com>
2020-03-14 19:18:13 -0400

    re-implement function predicate_ld_byte
    to avoid the seg fault when randomly sampling.
    s.addr may be empty since the safety has not been implemented

--- src/isa/ebpf/inst_codegen.cc
+++ src/isa/ebpf/inst_codegen.cc
@@ -1,11 +1,11 @@
 #include "inst_codegen.h"
 
+// Assume the input is safe, eg. addr+off can be found in s.addr
 z3::expr predicate_ld_byte(z3::expr addr, z3::expr off, smt_stack& s, z3::expr out) {
-  int i = s.addr.size() - 1;
   z3::expr a = addr + off;
-  z3::expr c = (a == s.addr[i]);
-  z3::expr f = z3::implies(c, out == s.val[i]);
-  for (i = i - 1; i >= 0; i--) {
+  z3::expr c = string_to_expr("false");
+  z3::expr f = string_to_expr("true");
+  for (int i = s.addr.size() - 1; i >= 0; i--) {
     f = f && z3::implies((!c) && (a == s.addr[i]), out == s.val[i]);
     c = c || (a == s.addr[i]);
   }
