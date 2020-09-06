050d7c6ce620ad44780637c36501c902b0fc6f79
Srinivas Narayana <ngsrinivas@gmail.com>
2020-09-06 17:30:40 -0400

    Fix bug in the read loop

--- z3server.cc
+++ z3server.cc
@@ -8,8 +8,8 @@
 
 #define FORMULA_SHM_KEY 224
 #define RESULTS_SHM_KEY 46
-#define FORMULA_SIZE_BYTES 65536
-#define RESULT_SIZE_BYTES  4096
+#define FORMULA_SIZE_BYTES 65535
+#define RESULT_SIZE_BYTES  65535
 
 #define PORT 8001
 
@@ -20,6 +20,7 @@ int read_problem_from_z3client();
 
 string run_solver(char* formula) {
   z3::solver s(c);
+  string res;
   s.from_string(formula);
   cout << "Checking... result:\n";
   switch (s.check()) {
@@ -28,9 +29,9 @@ string run_solver(char* formula) {
       return "unsat";
     }
     case z3::sat: {
-      /* TODO: extract a model and pass that as the result instead */
-      cout << "sat\n";
-      return "sat";
+      res = Z3_model_to_string(c, s.get_model());
+      cout << res;
+      return res;
     }
     case z3::unknown: {
       cout << "unknown\n";
@@ -74,15 +75,17 @@ int read_problem_from_z3client() {
       exit(EXIT_FAILURE);
     }
 
+    cout << "Received a new connection. Reading formula...\n";
     /* Read the full formula into buffer. */
     total_read = 0;
     do {
-      nread = read(acc_socket, buffer, FORMULA_SIZE_BYTES - total_read);
+      nread = read(acc_socket, buffer + total_read, FORMULA_SIZE_BYTES - total_read);
       total_read += nread;
-    } while (buffer[total_read] != '\0');
-
-    cout << "Formula from client:\n";
-    cout << buffer << endl;
+    } while (buffer[total_read - 1] != '\0' &&
+             total_read <= FORMULA_SIZE_BYTES);
+    if (total_read >= FORMULA_SIZE_BYTES)
+      cout << "Exhausted formula read buffer\n";
+    cout << "Formula from client:\n" << buffer << endl;
 
     /* Run the solver. */
     result = run_solver(buffer);
@@ -91,8 +94,9 @@ int read_problem_from_z3client() {
     res_buffer[nchars] = '\0';
 
     /* Send result. */
-    send(acc_socket, res_buffer, strlen(res_buffer), 0);
+    send(acc_socket, res_buffer, nchars + 1, 0);
     cout << "Result sent from server -> client: " << res_buffer << "\n";
+    close(acc_socket);
   }
   return 0;
 }
