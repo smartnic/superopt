88a2a4466f64a14fc8259ac85e28a810685ace27
QiongwenXu <qwxu@outlook.com>
2020-09-17 19:52:48 -0500

    set initial live register, program start/end as input parameters

--- src/isa/ebpf/inst.h
+++ src/isa/ebpf/inst.h
@@ -674,4 +674,5 @@ class inst: public inst_base {
 
 void interpret(inout_t& output, inst* program, int length, prog_state &ps, const inout_t& input);
 void safety_chk(inst& insn, prog_state& ps);
-void liveness_analysis(vector<unordered_set<int>>& live_regs, inst* program, int len);
+void liveness_analysis(unordered_set<int>& live_regs, inst* program, int start, int end,
+                       const unordered_set<int>& initial_live_regs);
