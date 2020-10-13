d8da498e0e5b5f5bdf44625ce2cf3f22379a4994
QiongwenXu <qwxu@outlook.com>
2020-10-13 14:58:32 -0500

    liveness analysis of register/pkt/stack

--- src/isa/ebpf/canonicalize.h
+++ src/isa/ebpf/canonicalize.h
@@ -16,14 +16,24 @@ struct register_state {
   int off;
 };
 
+struct live_variables {
+  unordered_set<int> regs;
+  // stack, pkt
+  unordered_map<int, unordered_set<int>> mem; // offsets in different memory ranges, todo: deal with map later
+};
+
 class inst_static_state {
  public:
   vector<vector<register_state>> reg_state; // all possible states of registers
+  live_variables live_var;
+
   inst_static_state();
   void copy_reg_state(int dst_reg, int src_reg);
   void set_reg_state(int reg, int type, int off = 0);
   void insert_reg_state(inst_static_state& iss);
+  void insert_live_var(inst_static_state& iss);
+  static intersection_live_var(inst_static_state& iss, inst_static_state& iss1, inst_static_state& iss2);
 };
 
 typedef vector<inst_static_state> prog_static_state;
-void static_analysis(inst* program, int len);
+void static_analysis(prog_static_state& pss, inst* program, int len);
